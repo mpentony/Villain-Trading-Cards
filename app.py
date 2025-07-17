7d5cde65502429bfb5f0288ee9ea176f30ce1fcc
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-16 22:32:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime  #to get the current date and time
 app = Flask("app")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///villain.db'
 db=SQLAlchemy(app)
+
+class Villain(db.Model):
+  id=db.Column(db.Integer, primary_key=True)
+  name=db.Column(db.String(80),unique=True,nullable=False)
+  description=db.Column(db.String(250),nullable=False)
+  interests=db.Column(db.String(250),nullable=False)
+  url=db.Column(db.String(250),nullable=False)
+  dateadded=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+
+def __repr__(self):
+  return "<Villain " + self.name + ">"
 
 @app.route("/")
 def hello_world():
