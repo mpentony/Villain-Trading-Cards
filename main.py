a2c5e4543e342681d3c6c8a6c2093cdabbcb19be
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-18 17:36:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timezone
-
-app = Flask("app")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///villain.db"
-db = SQLAlchemy(app)
-timezone=timezone.utc
-
-class Villain(db.Model):
-  id=db.Column(db.Integer, primary_key=True)
-  name=db.Column(db.String(80), unique=True, nullable=False)
-  description=db.Column(db.String(250), unique=True, nullable=False)
-  interests=db.Column(db.String(250), unique=True, nullable=False)
-  url=db.Column(db.String(250), nullable=False)
-  date_added=db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
-
-  def __repr__(self):
-    return f"<Villain {self.name}>"
-    
-  
 
 app = Flask("app")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///villain.db"
