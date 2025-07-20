aa3f09800dea2f08458c135b820a3df2b197d8a4
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-20 16:45:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,67 +16,68 @@ class Villain(db.Model):
 
     def __repr__(self):
         return "<Villain "+ self.name + ">"
-
+        
 with app.app_context():
 	db.create_all()
 	db.session.commit()
-  
+
 #### Serving Static Files
 @app.route("/")
 def villain_cards():
   return app.send_static_file("villain.html")
 
-#@app.route("/add")
-#def add():
-#  return app.send_static_file("addvillain.html")
+@app.route("/add")
+def add():
+  return app.send_static_file("addvillain.html")
 
-#@app.route("/delete")
-#def delete():
-#  return app.send_static_file("deletevillain.html")
+@app.route("/delete")
+def delete():
+  return app.send_static_file("deletevillain.html")
 ####
 
-#ADD CODE: add /api/villains route here
 @app.route("/api/villains/", methods=["GET"])
 def get_villains():
-  villains = Villain.query.all()
+  villains=Villain.query.all()
   data = []
   return
 
-
-
 @app.route("/api/villains/add", methods=["POST"])
 def add_villain():
-  errors = []
-  name = request.form.get("name")
+ errors = []
+ name = request.form.get("name", "")
 
-  if not name:
-    errors.append("Oops! Looks like you forgot a name!")
-
-  description = request.form.get("description")
+ if not name:
+  errors.append("Oops! Looks like you forgot a name!")
+  
+  description = request.form.get("description", "")
   if not description:
     errors.append("Oops! Looks like you forgot a description!")
   
-  interests = request.form.get("interests")
+  interests = request.form.get("interests", "")
   if not interests:
     errors.append("Oops! Looks like you forgot some interests!")
   
-  url = request.form.get("url")
+  url = request.form.get("url", "")
   if not url:
     errors.append("Oops! Looks like you forgot an image!")
-  
+      
   villain = Villain.query.filter_by(name=name).first()
   if villain:
     errors.append("Oops! A villain with that name already exists!")
-  
+      
   if errors:
     return jsonify({"errors": errors})
   else:
-    new_villain = Villain(name=name,description=description, interests=interests, url=url)
+    new_villain = Villain(name=name,
+    description=description,
+    interests=interests,
+    url=url
+    )
     db.session.add(new_villain)
     db.session.commit()
     return 
 
-@app.route("/api/villains/delete", methods=["POST"])
+@app.route("/api/villains/delete/", methods=["POST"])
 def delete_villain():
   name = request.form.get("name", "")
   villain = Villain.query.filter_by(name=name).first()
@@ -87,5 +88,13 @@ def delete_villain():
   else:
     return jsonify({"errors": ["Oops! A villain with that name doesn't exist!"]})
 
+@app.route("/api/", methods=["GET"])
+def get_endpoints():
+    endpoints = {
+        "/api/villains/": "GET - Retrieves all villain data from the database",
+        "/api/villains/delete": "POST - Deletes a single villain if the villain exists in the database",
+        "/api/villains/add": "POST - Adds a single villain to the database"
+    }
+    return jsonify(endpoints)
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host="0.0.0.0", port=8080)
